a4ec62ecb378ba4164297fb1f12a6103e27f6765
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-20 18:26:15 -0400

    feat: cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('should render the App component', () => {
+    render(<App />);
+
+    const app = screen.getByTestId('app');
+    expect(app).toBeInTheDocument();
+  });
 });
